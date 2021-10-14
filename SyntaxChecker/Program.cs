d6e93a4b0ac71104acead676be74e54ce3853bf5
Ryan Taylor <taylor.john.ryan@gmail.com>
2021-10-14 19:55:31 -0400

    start

--- SyntaxChecker/Program.cs
+++ SyntaxChecker/Program.cs
@@ -1,3 +1,27 @@
 ﻿//namespace SyntaxDefinitions;
 
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+string root = Directory.GetCurrentDirectory();
+StreamReader stream;
+
+foreach ( string s in Directory.GetFiles(root) ) {
+
+  if ( s.EndsWith("xml") || s.EndsWith("json") )
+    stream = File.OpenText(s);
+  else
+    continue;
+
+  string stringfile = stream.ReadToEnd();
+
+  var RXOpt = RegexOptions.Multiline | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace;
+
+  Regex RegExDraftsJSONIntact = new Regex("" +
+    "\\A", RXOpt) {
+
+  };
+}
+
 System.Console.WriteLine("Hello, World!");
