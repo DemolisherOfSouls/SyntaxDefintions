ba12948302dda6f323e94bc23283164929c702b1
Grincess <taylor.john.ryan@gmail.com>
2022-02-21 19:21:31 -0500

    npp schema minor addition - colorStyle

--- SyntaxChecker/Program.cs
+++ SyntaxChecker/Program.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace SyntaxChecker;
@@ -10,30 +9,37 @@ namespace SyntaxChecker;
 internal class Program
 {
   [STAThread]
-  private static int Main (string[] args) {
-    foreach ( string s in Directory.GetFiles(V.Root) ) {
+  private static int Main (string[] args)
+  {
+    foreach ( string s in Directory.GetFiles(V.Root) )
+    {
 
       bool IsXML = s.EndsWith("xml");
       bool IsJSON = s.EndsWith("json");
 
-      if ( IsXML || IsJSON ) {
+      if ( IsXML || IsJSON )
+      {
         V.Streamer = File.OpenText(s);
       }
-      else {
+      else
+      {
         continue;
       }
 
       string stringfile = V.Streamer.ReadToEnd();
 
-      if ( IsXML ) {
+      if ( IsXML )
+      {
         List<List<string>> KeywordList = new ();
         MatchCollection keywords = V.RegExNppKeywords.Matches(stringfile);
         //int gnum = V.RegExNppKeywords.GroupNumberFromName("group");
         //int knum = V.RegExNppKeywords.GroupNumberFromName("keyword");
-        foreach ( Match match in keywords ) {
+        foreach ( Match match in keywords )
+        {
           CaptureCollection captures = match.Captures;
           List <string> caplist = captures.ToList();
-          foreach ( Capture c in captures ) {
+          foreach ( Capture c in captures )
+          {
 
             caplist.Add(c.Value);
           }
@@ -41,7 +47,7 @@ internal class Program
         }
 
         //Generate
-        string output = "";
+        //string output = "";
       }
     }
 
