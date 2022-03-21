97a3079497a8da7ea280a8a5d2a4c90dfb84f996
Grincess <taylor.john.ryan@gmail.com>
2022-03-21 03:20:06 -0400

    schema moved

--- Fetch.cmd
+++ Fetch.cmd
@@ -4,4 +4,6 @@ cd "G:\My Drive\Projects\Notepad++\userDefineLangs"
 xcopy *.xml "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Notepad++ Syntax\" /y
 cd Storage
 xcopy *.xml "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Notepad++ Syntax\" /y
-cd "..\"
+
+cd "C:\Users\taylo\iCloudDrive\iCloud~com~agiletortoise~Drafts5\Library\Syntaxes"
+xcopy *.json "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Drafts\" /y
