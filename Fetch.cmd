db43d3308f6be7520bf9fa2b312c393a2771e0da
Grincess <taylor.john.ryan@gmail.com>
2022-03-21 07:30:39 -0400

    typo

--- Fetch.cmd
+++ Fetch.cmd
@@ -6,4 +6,4 @@ cd Storage
 xcopy *.xml "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Notepad++ Syntax\" /y
 
 cd "C:\Users\taylo\iCloudDrive\iCloud~com~agiletortoise~Drafts5\Library\Syntaxes"
-xcopy *.json "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Drafts\" /y
+xcopy *.json "G:\My Drive\Projects\Visual Studio\SyntaxDefintions\Drafts Syntax\" /y
