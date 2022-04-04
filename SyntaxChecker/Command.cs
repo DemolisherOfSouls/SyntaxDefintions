02cd826e66226bd4f08d995d2a5e3ef655b2bd39
Grincess <taylor.john.ryan@gmail.com>
2022-04-04 01:57:16 -0400

    restructure

---
+++ SyntaxChecker/Command.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+
+namespace SyntaxChecker;
+
+internal class Command
+{
+  public string Name { get; private set; }
+  public Delegate Action { get; private set; }
+
+  Command () : this("Undefined") { }
+  Command (string s) : this(s, null) { }
+  Command (string s, Delegate d)
+  {
+    Name = s;
+    Action = d;
+  }
+
+  public void Execute (object caller, string[] args)
+  {
+    Action.Method.Invoke(caller, args);
+  }
+}
--- SyntaxChecker/Program.cs
+++ SyntaxChecker/Program.cs
@@ -7,25 +7,6 @@ using System.Xml;
 
 namespace SyntaxChecker;
 
-internal class Command
-{
-  public string Name { get; private set; }
-  public Delegate Action { get; private set; }
-
-  Command () : this("Undefined") { }
-  Command (string s) : this(s, null) { }
-  Command (string s, Delegate d)
-  {
-    Name = s;
-    Action = d;
-  }
-
-  public void Execute (object caller, string[] args)
-  {
-    Action.Method.Invoke(caller, args);
-  }
-}
-
 internal class Program
 {
   private static void LoadSettings ()
